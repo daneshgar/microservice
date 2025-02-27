ef17a9a23fb3f65d39cf27715ea94aefcc184687
Alireza Shabani <shabani@PouyaAndishan.local>
2025-02-27 13:52:23 +0330

    add new version

---
+++ Src/Services/IDpService/IDP.Api/Controllers/V1/UserController.cs
@@ -0,0 +1,12 @@
+﻿using IDP.Api.Controllers.BaseController;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.V1
+{
+    [Route("api/V1/User")]
+    [ApiController]
+    public class UserController : IBaseController
+    {
+    }
+}
